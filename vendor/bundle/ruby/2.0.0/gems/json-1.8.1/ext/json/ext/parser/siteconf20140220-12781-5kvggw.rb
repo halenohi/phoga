cc0713da0a26c62cbb19a24180c841c54ce6d739
nnnzzz000 <morita.nozomi@halenohi.jp>
2014-02-21 14:29:26 +0900

    2/21作業分

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.1/ext/json/ext/parser/siteconf20140220-12781-5kvggw.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20140220-12781-sd72vu"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
