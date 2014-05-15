dc930f68983fe1c95666d17becb0c45e9cafc9e2
atsu matsuda <achm.pppp@gmail.com>
2014-05-15 14:50:07 +0900

    Phoga::Tagのslug保存時にURLエンコードをするように修正

--- app/models/phoga/tag.rb
+++ app/models/phoga/tag.rb
@@ -1,10 +1,19 @@
+require 'cgi'
+
 module Phoga
   class Tag < ActiveRecord::Base
+    before_save :url_encode
+
     has_many :taggings,
       dependent: :destroy
 
     has_many :tagged_articles,
       through: :taggings,
       source: :article
+
+    private
+      def url_encode
+        self.slug = CGI.escape self.slug
+      end
   end
 end
